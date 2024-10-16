1ea4328e6246837e59586c6768699cbcb482a49c
lukaNikolava <nikolavaluka475@gmail.com>
2024-10-16 16:32:16 +0400

    FINAL

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function adjustBannerSize() {
     const banner = document.querySelector('.banner');
-    const height = window.innerHeight;
-    const width = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
 
-    
-    banner.style.width = '500px';
-    banner.style.height = '1000px';
-
-    
-    if (height < 1000) {
-        banner.style.height = `${height}px`;
-        banner.style.width = `${height / 2}px`;
-    } 
-    
-    
-    if (width < 500) {
-        banner.style.width = `${width}px`;
-        banner.style.height = `${width * 2}px`;
+    if (viewportHeight < 1000) {
+        banner.style.height = `${viewportHeight}px`;
+        banner.style.width = `${viewportHeight / 2}px`;
+    } else if (viewportWidth < 500) {
+        banner.style.width = `${viewportWidth}px`;
+        banner.style.height = `${viewportWidth * 2}px`;
+    } else {
+        banner.style.width = '500px';
+        banner.style.height = '1000px';
     }
 }
 
